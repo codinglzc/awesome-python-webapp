a2538e2cd814aa8c3d54deff810e113efe83b31b
lzc <822688424@qq.com>
2017-09-11 14:28:00 +0800

    add database module

---
+++ www/transwarp/db.py
@@ -0,0 +1,406 @@
+# coding=utf-8
+
+__author__ = "Liu Cong"
+
+"""
+filename: db.py
+create time: 2017-09-09
+disc: 数据库模块，使用mysql数据库。
+"""
+
+import time
+import uuid
+import logging
+import doctest
+import threading
+import functools
+import mysql.connector
+
+class Dict(dict):
+    """
+    在原dict基础上添加了一些功能：
+    可以以面向对象的思想调用，如d['key']和d.key都可以得到value。
+    """
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+def next_id(t=None):
+    """
+    根据时间戳和随机数生成 uid 标识符
+    :param t: unix timestamp, default to None and using time.time().
+    :return: Return next id as 50-char string.
+    """
+    if t is None:
+        t = time.time()
+    return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
+
+# def _profiling(start, sql=''):
+#     t = time.time() - start
+#     if t > 0.1:
+#         logging.warning('[PROFILING] [DB] %s: %s' % (t, sql))
+#     else:
+#         logging.info('[PROFILING] [DB] %s: %s' % (t, sql))
+
+class DBError(Exception):
+    pass
+
+class MultiColumnsError(DBError):
+    pass
+
+class _LasyConnection(object):
+
+    def __init__(self):
+        self.connection = None
+
+    def cursor(self):
+        if self.connection is None:
+            conn = engine.connect()
+            logging.info('open connection <%s>...' % hex(id(conn)))
+            self.connection = conn
+        return self.connection.cursor()
+
+    def commit(self):
+        self.connection.commit()
+
+    def rollback(self):
+        self.connection.rollback()
+
+    def cleanup(self):
+        if self.connection:
+            conn = self.connection
+            self.connection = None
+            logging.info('close connection <%s>...' % hex(id(conn)))
+            conn.close()
+
+class _DbCtx(threading.local):
+    """
+    持有数据库连接的上下文对象
+    该类继承于threading.local，应用于多线程中，使每一个线程都有独立的属性，且互不影响。
+    """
+    def __init__(self):
+        self.connection = None
+        self.transactions = 0
+
+    def is_init(self):
+        return self.connection is not None
+
+    def init(self):
+        logging.info('open lazy connection...')
+        self.connection = _LasyConnection()
+        self.transactions = 0
+
+    def cleanup(self):
+        self.connection.cleanup()
+        self.connection = None
+
+    def cursor(self):
+        return self.connection.cursor()
+
+_db_ctx = _DbCtx()  # _db_ctx对象对每个线程看到的都不是一样的。
+
+engine = None  # global engine object:
+
+class _Engine(object):
+    """
+    数据库引擎对象
+    属性connect是一个函数。
+    """
+    def __init__(self, connect):
+        self._connect = connect
+
+    def connect(self):
+        return self._connect()
+
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initialized.')
+    params = dict(
+        user=user
+        , password=password
+        , database=database
+        , host=host
+        , port=port
+    )
+    defaults = dict(
+        use_unicode=True
+        , charset='utf8'
+        , collation='utf8_general_ci'
+        , autocommit=False
+    )
+    # 对照defaults字典里面的元素把参数kw字典里面相应的元素添加到params中
+    for k, v in defaults.iteritems():
+        params[k] = kw.pop(k, v)
+    # 再把剩余的kw元素加到params中
+    params.update(kw)
+    params['buffered'] = True
+    engine = _Engine(lambda: mysql.connector.connect(**params))
+    logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
+
+class _ConnectionCtx(object):
+    """
+    数据库连接的上下文，目的是自动获取和释放连接
+    定义了__enter__()和__exit__()的对象可以用于with语句。
+    """
+    def __enter__(self):
+        global _db_ctx
+        self.should_cleanup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_cleanup = True
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+def connection():
+    return _ConnectionCtx()
+
+# 装饰器--connection
+def with_connection(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        with _ConnectionCtx():
+            return func(*args, **kw)
+    return _wrapper
+
+class _TransactionCtx(object):
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        logging.info('begin transaction...' if _db_ctx.transactions == 1 else 'join current transaction...')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions == 0:
+                if exc_type is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        logging.info('commit transaction...')
+        try:
+            _db_ctx.connection.commit()
+            logging.info('commit ok.')
+        except:
+            logging.warning('commit failed. try rollback...')
+            _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        logging.warning('rollback transaction...')
+        _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
+
+def transaction():
+    return _TransactionCtx()
+
+def with_transaction(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        # _start = time.time()
+        with _TransactionCtx():
+            return func(*args, **kw)
+        # _profiling(_start)
+    return _wrapper
+
+def _select(sql, first, *args):
+    """
+    执行查询SQL语句，并根据first值来决定返回一个结果还是list结果集。
+    :param sql: 查询sql语句，参数用?代替。
+    :param first: bool值，是否只取结果集中的第一条记录。
+    :param args: sql语句中?的参数。
+    :return:
+    """
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        # 得到字段的名字
+        names = []
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        # 如果first为true，则返回第一个结果
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
+
+@with_connection
+def select_one(sql, *args):
+    '''
+    Execute select SQL and expected one result.
+    If no result found, return None.
+    If multiple results found, the first one returned.
+    >>> u1 = dict(id=100, name='Alice', email='alice@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> u2 = dict(id=101, name='Sarah', email='sarah@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> u = select_one('select * from user where id=?', 100)
+    >>> u.name
+    u'Alice'
+    >>> select_one('select * from user where email=?', 'abc@email.com')
+    >>> u2 = select_one('select * from user where passwd=? order by email', 'ABC-12345')
+    >>> u2.name
+    u'Alice'
+    '''
+    return _select(sql, True, *args)
+
+@with_connection
+def select_int(sql, *args):
+    '''
+    Execute select SQL and expected one int and only one int result.
+    >>> n = update('delete from user')
+    >>> u1 = dict(id=96900, name='Ada', email='ada@test.org', passwd='A-12345', last_modified=time.time())
+    >>> u2 = dict(id=96901, name='Adam', email='adam@test.org', passwd='A-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> select_int('select count(*) from user')
+    2
+    >>> select_int('select count(*) from user where email=?', 'ada@test.org')
+    1
+    >>> select_int('select count(*) from user where email=?', 'notexist@test.org')
+    0
+    >>> select_int('select id from user where email=?', 'ada@test.org')
+    96900
+    >>> select_int('select id, name from user where email=?', 'ada@test.org')
+    Traceback (most recent call last):
+        ...
+    MultiColumnsError: Expect only one column.
+    '''
+    d = _select(sql, True, *args)
+    if len(d) != 1:
+        raise MultiColumnsError('Expect only one column.')
+    return d.values()[0]
+
+@with_connection
+def select(sql, *args):
+    '''
+    Execute select SQL and return list or empty list if no result.
+    >>> u1 = dict(id=200, name='Wall.E', email='wall.e@test.org', passwd='back-to-earth', last_modified=time.time())
+    >>> u2 = dict(id=201, name='Eva', email='eva@test.org', passwd='back-to-earth', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> L = select('select * from user where id=?', 900900900)
+    >>> L
+    []
+    >>> L = select('select * from user where id=?', 200)
+    >>> L[0].email
+    u'wall.e@test.org'
+    >>> L = select('select * from user where passwd=? order by id desc', 'back-to-earth')
+    >>> L[0].name
+    u'Eva'
+    >>> L[1].name
+    u'Wall.E'
+    '''
+    return _select(sql, False, *args)
+
+@with_connection
+def _update(sql, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        r = cursor.rowcount
+        if _db_ctx.transactions == 0:
+            # no transactions enviroment:
+            logging.info('auto commit')
+            _db_ctx.connection.commit()
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+def insert(table, **kw):
+    '''
+    Execute insert SQL.
+    >>> u1 = dict(id=2000, name='Bob', email='bob@test.org', passwd='bobobob', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> u2 = select_one('select * from user where id=?', 2000)
+    >>> u2.name
+    u'Bob'
+    >>> insert('user', **u2)
+    Traceback (most recent call last):
+      ...
+    IntegrityError: 1062 (23000): Duplicate entry '2000' for key 'PRIMARY'
+    '''
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
+def update(sql, *args):
+    r'''
+    Execute update SQL.
+    >>> u1 = dict(id=1000, name='Michael', email='michael@test.org', passwd='123456', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> u2 = select_one('select * from user where id=?', 1000)
+    >>> u2.email
+    u'michael@test.org'
+    >>> u2.passwd
+    u'123456'
+    >>> update('update user set email=?, passwd=? where id=?', 'michael@example.org', '654321', 1000)
+    1
+    >>> u3 = select_one('select * from user where id=?', 1000)
+    >>> u3.email
+    u'michael@example.org'
+    >>> u3.passwd
+    u'654321'
+    >>> update('update user set passwd=? where id=?', '***', '123\' or id=\'456')
+    0
+    '''
+    return _update(sql, *args)
+
+if __name__ == '__main__':
+    # 设置日志级别，日志只会打印小于等于这个级别的日志
+    logging.basicConfig(level=logging.DEBUG)
+    create_engine('root', '123456', 'awesome_python_webapp')
+    update('drop table if exists user')
+    update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
+    # 测试模块，可以把每个函数里面的shell尝试运行一遍
+    # doctest.testmod()
